269de6b0127082ba020edd543d9bfff26a19a263
Aaditya Mathur <104914201+adityamathur456@users.noreply.github.com>
2026-07-05 13:03:09 +0530

    Update API base URLs to new server addresses

--- account-management-client/app/api/axios.ts
+++ account-management-client/app/api/axios.ts
@@ -2,13 +2,13 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 
 // Create main API instance
 export const api = axios.create({
-  baseURL: "http://34.9.62.254:8085/api",
+  baseURL: "http://34.60.148.162:8085/api",
   headers: { "Content-Type": "application/json" },
 });
 
 // Create auth API instance
 export const authApi = axios.create({
-  baseURL: "http://34.9.62.254:8085/auth",
+  baseURL: "http://34.60.148.162:8085/auth",
   headers: { "Content-Type": "application/json" },
 });
 
